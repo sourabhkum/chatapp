aba2f89584c6f3bcece51239e77a41b9e8198219
sourabh <sourabhcybo@gmail.com>
2017-11-23 14:14:32 +0530

    add message emit on create

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,7 @@
 var socket=io();
 socket.on('connect',function(){
     console.log('connected to server');
-    socket.emit('createMessage',{
-        from:'sourabh',
-        message:'hi there'
-    });
+    
 });
 socket.on('disconnect',function(){
     console.log('disconnected to server');
--- server/server.js
+++ server/server.js
@@ -1,31 +1,33 @@
-const path =require('path');
-const express=require('express');
-const http=require('http');
-const socketIO=require('socket.io');
-const publicPath=path.join(__dirname,'../public');
-const port=process.env.PORT||3000;
-var app=express();
-var server=http.createServer(app);
-var io=socketIO(server);
+const path = require('path');
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
-io.on('connection',(socket)=>{
+io.on('connection', (socket) => {
     console.log('New user Connected');
-    socket.on('disconnect',()=>{
+    
+    socket.on('createMessage', (newMessage) => {
+        console.log('createMessage', newMessage)
+        io.emit('newMessage',{
+            from:newMessage.from,
+            text:newMessage.text
+        });
+    });
+
+    socket.on('disconnect', () => {
         console.log('user Disconnected');
     });
-    socket.emit('newMessage',{
-        from:'sourabhcybo@gmail.com',
-        text:'hi this is text',
-        id:129
-    });
-    
-    socket.on('createMessage',(newMessage)=>{
-        console.log('createMessage',newMessage)
-    });
+
 });
 
 
-server.listen(port,()=>{
+server.listen(port, () => {
     console.log(`server up an running ${port} port`);
 });
