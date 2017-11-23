b90d2abc4e60b662f96c65e1b5dfebd3b78d6072
sourabh <sourabhcybo@gmail.com>
2017-11-23 16:54:11 +0530

    add form for message and display incoming messages

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,24 @@
-var socket=io();
-socket.on('connect',function(){
+var socket = io();
+socket.on('connect', function () {
     console.log('connected to server');
-    
+
 });
-socket.on('disconnect',function(){
+socket.on('disconnect', function () {
     console.log('disconnected to server');
 });
-socket.on('newMessage',function(message){
-console.log('newMessage',message)
+socket.on('newMessage', function (message) {
+    console.log('newMessage', message)
+    var li=$('<li></li>');
+    li.text(`${message.from}:${message.text}`);
+    $('#messages').append(li);
+});
+
+$('#message-from').on('submit',function(e){
+    e.preventDefault();
+    socket.emit('createMessage',{
+        from:'User',
+        text:$('[name=newMessage]').val()
+    },function(){
+
+    });
 });
--- server/server.js
+++ server/server.js
@@ -18,16 +18,16 @@ io.on('connection', (socket) => {
 
     socket.broadcast.emit('newMessage',generateMessage('Admin','New user Joined'));
 
-    socket.on('createMessage', (newMessage) => {
+    socket.on('createMessage', (newMessage,callback) => {
         console.log('createMessage', newMessage)
         io.emit('newMessage', generateMessage(newMessage.from,newMessage.text));
         //socket.broadcast.emit('newMessage',generateMessage(newMessage.from,newMessage.text));
+        callback('this is from server');
     });
 
     socket.on('disconnect', () => {
         console.log('user Disconnected');
     });
-
 });
 
 
